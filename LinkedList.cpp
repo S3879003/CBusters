d82417a60798e25f038667dca56deb017172758f
Michael Tamasauskas <s3562292@student.rmit.edu.au>
2022-01-17 22:41:03 +1100

    Working on addFront

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -3,9 +3,42 @@
 
 LinkedList::LinkedList() {
    head = nullptr;
-
+   
    // TODO
+   tail = nullptr;
+   length = 0;
 }
 
 LinkedList::~LinkedList() {
+   delete head;
+   delete tail;
+}
+
+void LinkedList::addFront(Tile* t) {
+   Node* temp = new Node(); // on the heap (to be deleted) DECONSTRUCTOR
+   temp->tile = t;
+   temp->next = NULL;
+   
+   if (head == NULL) {
+      head = temp;
+      tail = temp;
+   }
+   else {
+      temp->next = head;
+      head = temp;
+   }
+   length++;
+   delete temp;
+}
+
+void LinkedList::removeFront() {
+
+}
+
+void LinkedList::addEnd(int n) {
+
+}
+
+void LinkedList::removeEnd() {
+
 }
--- LinkedList.h
+++ LinkedList.h
@@ -10,8 +10,15 @@ public:
    LinkedList();
    ~LinkedList();
 
+   void addFront(int n);
+   void removeFront();
+   void addEnd(int n);
+   void removeEnd();
+
 private:
    Node* head;
+   Node* tail;
+   int length;
 };
 
 #endif // ASSIGN2_LINKEDLIST_H
