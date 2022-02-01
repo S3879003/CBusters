a3a3b19ca29cc55bc34455fb061d1cdde3dad8d8
Michael Tamasauskas <s3562292@student.rmit.edu.au>
2022-02-01 22:45:24 +1100

    Basic menu output and switch statement

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -9,7 +9,37 @@ int main(void) {
    LinkedList* list = new LinkedList();
    delete list;
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
+   bool inGame = true;
+   int menuSelection = 0;
+
+   while (inGame != false) {
+      std::cout << "Menu" << std::endl;
+      std::cout << "----";
+      std::cout << " 1. New Game" << std::endl;
+      std::cout << " 2. Load Game" << std::endl;
+      std::cout << " 3. Credits (Show student information)" << std::endl;
+      std::cout << " 4. Quit" << std::endl;
+
+      std::cin >> menuSelection;   
+
+      switch (menuSelection) {
+         case 1:
+         // TODO
+         break;
+         case 2:
+         // TODO
+         break;
+         case 3:
+         // TODO
+         break;
+         case 4:
+         inGame = false;
+         break;
+         default:
+         std::cout << "Not a Valid Choice. Enter the corresponding menu number" << std::endl;
+         std::cin >> menuSelection;
+         break;
+   }
 
    return EXIT_SUCCESS;
 }
